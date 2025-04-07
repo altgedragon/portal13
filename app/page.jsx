2819a96bac9939cb2439972b7414ae277c035dfe
altgedragon <milesthebrokenboy@gmail.com>
2025-04-07 01:02:42 -0500

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,14 @@
 export default function Home() {
-  return null; // Empty page, just showing the background
+  return (
+    <div className="container">
+      <div className="square top-square">Sample Title 1</div>
+      
+      <div className="middle-squares">
+        <div className="square middle-square">Sample 2</div>
+        <div className="square middle-square">Sample 3</div>
+      </div>
+      
+      <div className="square bottom-square">Sample 4</div>
+    </div>
+  )
 }
