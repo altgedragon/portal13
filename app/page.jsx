26d39fcfc0b6a15bbe7fff57848fee19ad777823
altgedragon <milesthebrokenboy@gmail.com>
2025-04-07 02:07:55 -0500

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -6,22 +6,30 @@ export default function Home() {
     adipiscing elit. Nullam auctor, nisl eget ultricies tincidunt. The five boxing wizards jump quickly. 
     Pack my box with five dozen liquor jugs. How vexingly quick daft zebras jump! Bright vixens jump; 
     dozy fowl quack. Jackdaws love my big sphinx of quartz. The jay, pig, fox, zebra and my wolves quack!
+    Blowzy red vixens fight for a quick jump. Joaquin Phoenix was gazed by MTV for luck. A wizard's job is 
+    to vex chumps quickly in fog. Watch "Jeopardy!", Alex Trebek's fun TV quiz game. Woven silk pyjamas 
+    exchanged for blue quartz. Brawny gods just flocked up to quiz and vex him. The quick onyx goblin jumps 
+    over the lazy dwarf. All questions asked by five watched experts amaze the judge. Back in June we delivered 
+    oxygen equipment of the same size. The public was amazed to view the quickness and dexterity of the juggler.
   `.repeat(5);
 
-  // Text for boxes 5-6
-  const boxFiveSixText = `
-    This is the content for Boxes 5 and 6 with green text. The background is now the same medium grey
-    as all other boxes, but the text color distinguishes these boxes. The container is significantly
-    larger than standard boxes but maintains the same styling principles.
+  // Expanded text for boxes 5-6
+  const expandedText = `
+    This is the non-scrolling content for Boxes 5 and 6 with light tan background.
+    The text color has been optimized for readability against the tan background.
     
-    [Section 1] Demonstration of colored text on unified background
-    [Section 2] All boxes share the #777777 background color
-    [Section 3] Only text colors differentiate certain boxes
+    Sample sections:
+    [1] The quick brown fox jumps over the lazy dog.
+    [2] Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    [3] Nullam auctor, nisl eget ultricies tincidunt.
+    [4] The five boxing wizards jump quickly.
+    [5] How vexingly quick daft zebras jump!
+    [6] Bright vixens jump; dozy fowl quack.
   `.repeat(2);
 
   return (
     <div className="container">
-      {/* Box 1 (Top) - Pink text */}
+      {/* Box 1 (Light Tan) */}
       <div className="square top-square">
         <div className="content-title">Sample Title 1</div>
         <div className="content-body">
@@ -29,15 +37,15 @@ export default function Home() {
         </div>
       </div>
 
-      {/* Box 6 - Green text */}
+      {/* Box 6 (Light Tan) */}
       <div className="square box-six">
         <div className="content-title">Sample Box 6</div>
         <div className="content-body">
-          <p>{boxFiveSixText}</p>
+          <p>{expandedText}</p>
         </div>
       </div>
       
-      {/* Boxes 2 & 3 (Middle) - Default text */}
+      {/* Boxes 2 & 3 (Light Caramel) */}
       <div className="middle-squares">
         <div className="square middle-square">
           <div className="content-title">Sample 2</div>
@@ -53,7 +61,7 @@ export default function Home() {
         </div>
       </div>
       
-      {/* Box 4 (Bottom) - Pink text */}
+      {/* Box 4 (Light Red) */}
       <div className="square bottom-square">
         <div className="content-title">Sample 4</div>
         <div className="content-body">
@@ -61,11 +69,11 @@ export default function Home() {
         </div>
       </div>
       
-      {/* Box 5 - Green text */}
+      {/* Box 5 (Light Tan) */}
       <div className="square box-five">
         <div className="content-title">Sample Box 5</div>
         <div className="content-body">
-          <p>{boxFiveSixText}</p>
+          <p>{expandedText}</p>
         </div>
       </div>
     </div>
